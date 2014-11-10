afa9f13f22238dddf5c2b123ced439f457aa00fc
Steven Truesdell <steventruesdell@Stevens-MacBook-Pro.local>
2014-11-09 21:12:47 -0700

    had to do some tweaking to the twitch code as our website runs HTTPS and they dont :sadface: Get with the times guys

--- server/config/express.js
+++ server/config/express.js
@@ -22,17 +22,17 @@ var mongoose       = require('mongoose');
 module.exports = function(app) {
   var env = app.get('env');
 
-  app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Credentials', true);
-  res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.set('Access-Control-Allow-Headers',
-    'X-Requested-With, Content-Type, Authorization');
-  if ('OPTIONS' === req.method) {
-    return res.send(200);
-    }
-  next();
-});
+//   app.all('*', function(req, res, next) {
+//   res.set('Access-Control-Allow-Origin', '*');
+//   res.set('Access-Control-Allow-Credentials', true);
+//   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+//   res.set('Access-Control-Allow-Headers',
+//     'X-Requested-With, Content-Type, Authorization');
+//   if ('OPTIONS' === req.method) {
+//     return res.send(200);
+//     }
+//   next();
+// });
 
 
   app.set('views', config.root + '/server/views');
