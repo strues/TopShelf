ea66858a5c72c5157a3990eb232ffc5658f3e944
Steve Truesdell <steven.truesdell@gmail.com>
2014-12-29 16:06:06 -0700

    moved the recruitment panel to its own directive and directory

---
+++ client/app/core/home/home.recruitingPanel/recruitingPanel.directive.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+function recruitingPanel() {
+    return {
+      templateUrl: 'app/core/home/home.recruitingPanel/recruitingPanel.tpl.html',
+      restrict: 'EA',
+      controller: function($scope, recruitmentFactory, socket, $log) {
+        recruitmentFactory.getRecruitment()
+        .success(function (recruitments) {
+          $scope.recruitments = recruitments;
+          //console.log('aside - postsFactory.getPosts()', posts);
+          socket.syncUpdates('recruitment', $scope.recruitments);
+        }).
+        error(function (error) {
+          $scope.status = 'Unable to Retrieve Recruitment Status: ' + error.message;
+          console.log($scope.status);
+        });
+      }
+    };
+  }
+
+angular
+  .module('topshelf.core')
+  .directive('recruitingPanel', recruitingPanel);
+})();
