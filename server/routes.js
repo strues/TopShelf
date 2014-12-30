02057523968cb720d5f2ea214048d8451390cf71
Steve Truesdell <steven.truesdell@gmail.com>
2014-12-30 16:11:32 -0700

    fixed an error caused by updating battlenet-api

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,6 @@
 
 var errors = require('./components/errors');
 var path = require('path');
-var bnet = require('battlenet-api')();
 var config      = require('./config/environment');
 
 module.exports = function(app) {
