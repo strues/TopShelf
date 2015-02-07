039e9aa321c1b1eacf5ad4c90166afb8142ea420
Steve Truesdell <steven.truesdell@gmail.com>
2015-02-06 22:14:41 -0700

    whoops this file might be important

---
+++ client/app/app.module.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+  /* @ngdoc object
+   * @name app
+   * @requires $urlRouterProvider
+   *
+   * @description main application file
+   *
+   */
+    angular.module('app.core', []);
+    angular.module('app.guild', []);
+    angular.module('app.account', []);
+    angular.module('app.admin', []);
+    angular.module('app', []);
+    /* @ngInject */
+    angular.module('app', [
+                  'ngStorage',
+                  'ngResource',
+                  'ngMessages',
+                  'ngCookies',
+                  'ngTouch',
+                  'ngSanitize',
+                  'ngAnimate',
+                  'ui.router',
+                  'ui.bootstrap',
+                  'textAngular',
+                  'vModal',
+                  'toastr',
+                  'ngFabForm',
+                  'ngReactGrid',
+                  'angular-loading-bar',
+                  'app.core',
+                  'app.account',
+                  'app.guild',
+                  'app.admin'
+
+    ]);
+    /* @ngInject */
+    function run ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }
+    angular
+        .module('app')
+        .run(run);
+})();
