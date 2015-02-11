e2fda59eb8be6ed07df98d4d41c4419856b89be5
Steve Truesdell <steven.truesdell@gmail.com>
2015-02-11 14:15:36 -0700

    user details aside working properly

--- client/app/admin/states/users/details/user-details.controller.js
+++ client/app/admin/states/users/details/user-details.controller.js
@@ -1,45 +1,56 @@
-(function () {
+(function() {
     'use strict';
 
-  /**
-   * @ngdoc object
-   * @name admin.recruitment.controller:ApplicationListCtrl
-   *
-   * @description
-   *
-   */
+    /**
+     * @ngdoc controller
+     * @name admin.states.controller:AdminUserDetailsCtrl
+     *
+     * @description shows the users profile and allows the
+     * information to be edited in an aside
+     *
+     */
 
-    function AdminUserDetailsCtrl($scope, $http, $stateParams, $location) {
-
-    var userId = $stateParams.id;
-
-    if (userId && userId.length > 0) {
-        $http.get('/api/users/' + userId)
-        .success(function(user) {
-            $scope.user = user;
-        });
-    }
-    $scope.toggle = false;
-    $scope.saveAUser = function() {
-        if (userId && userId.length > 0) {
-            $http.put('/api/users/' + userId,
-              $scope.user).success(function(user) {
-                $location.path('/admin/users/details');
-            });
-        }
-        else {
-            $http.post('/api/users', $scope.user)
-              .success(function(user) {
-                $location.path('/admin/users/details');
-            });
-        }
-    };
-
-    $http.get('/api/users').success(function(users) {
-        $scope.users = users;
-    });
-}
     angular
         .module('app.admin.states')
         .controller('AdminUserDetailsCtrl', AdminUserDetailsCtrl);
+
+    function AdminUserDetailsCtrl($scope, $http, toastr, $aside, $stateParams, $location) {
+
+        var userId = $stateParams.id;
+
+        if (userId && userId.length > 0) {
+            $http.get('/api/users/' + userId)
+                .success(function(user) {
+                    $scope.user = user;
+                });
+        }
+
+        $scope.saveAUser = function() {
+            if (userId && userId.length > 0) {
+                $http.put('/api/users/' + userId,
+                    $scope.user).success(function(user) {
+                    toastr.success('Your changes have been saved');
+                });
+            } else {
+                $http.post('/api/users', $scope.user)
+                    .success(function(user) {
+                        toastr.success('Your changes have been saved');
+                    });
+            }
+        };
+
+        $http.get('/api/users').success(function(users) {
+            $scope.users = users;
+        });
+
+        var userDetailsEditAside = $aside({
+            scope: $scope,
+            template: 'app/admin/states/users/details/user-details-aside.tpl.html',
+            show: false
+        });
+        userDetailsEditAside.$promise.then(function() {
+            userDetailsEditAside.hide();
+        })
+    }
+
 })();
