d2ed0a419f608ce3119d76908d6d8bbb4770a2e5
Steve Truesdell <steven.truesdell@gmail.com>
2015-02-23 11:45:03 -0700

    removed news from modal and placed it on its own page

--- client/app/core/states/core.states.js
+++ client/app/core/states/core.states.js
@@ -19,6 +19,13 @@
             url: '/',
             templateUrl: 'app/core/states/home/core.home.tpl.html',
             controller: 'HomeCtrl as vm'
+        })
+        .state('viewPost', {
+          url: '/:id',
+          templateUrl: 'app/core/states/home/details/details.home.tpl.html',
+          controller: function($stateParams) {
+                            console.log($stateParams);
+                        }
         });
     }
 
---
+++ client/app/core/states/home/details/details.controller.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+  /**
+   * @name HomePostDetailsCtrl as carousel
+   * @desc Carousel controller
+   * @requires (mgcrea.ngStrap)
+   * @memberOf topshelf.core.states
+   */
+
+    angular
+        .module('app.core.states')
+        .controller('HomePostDetailsCtrl', HomePostDetailsCtrl);
+
+    function HomePostDetailsCtrl ($scope, $stateParams, $http, $timeout, $location) {
+        var hdetail = this;
+        var postId = $stateParams.id;
+
+        if (postId && postId.length > 0) {
+            $http.get('/api/posts/' + postId)
+                .success(function(post) {
+                    $scope.post = post;
+                });
+        }
+
+        $http.get('/api/posts').success(function(data) {
+            $scope.userData = data;
+        });
+  }
+})();
